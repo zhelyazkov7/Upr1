0a72a0bb4263d9afe1db48832d1c0b2729fb5342
zhelyazkov7 <80040886+zhelyazkov7@users.noreply.github.com>
2021-03-04 13:30:32 +0200

    Add files via upload

---
+++ Upr1/zad2.js
@@ -0,0 +1,16 @@
+function calculate(arr) {
+    let sum = 0;
+
+    for(let price of arr) {
+        sum += Number(price);
+    }
+	
+	let vat = sum * 0.2;
+	let total = sum * 1.2;
+
+    console.log(`SUM = ${sum}`);
+    console.log(`VAT = ${vat}`);
+    console.log(`TOTAL = ${total}`);
+}
+
+calculate(['1.20', '2.60', '3.50']);
